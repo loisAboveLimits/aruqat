6ca5ab00b5060fe4cbed33bbb1af9a1856732a21
microdetails <lois@micro-details.com>
2026-07-14 11:10:11 +0300

    restore controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,29 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use RalphJSmit\Laravel\SEO\Support\SEOData;
-
-
 abstract class Controller
 {
-
-    public function show()
-        {
-            seo()->for(
-                new SEOData(
-                    title: 'Home Page',
-                    description: 'Welcome to our website.',
-                    image: asset('images/logo.png'),
-                    author: 'Aruqat',
-                )
-            );
-
-            return view('home');
-        }
-
-
-
+    //
 }
-
-
-
