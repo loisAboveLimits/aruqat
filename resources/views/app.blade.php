0c2ef2320da66cadd39754f0f4cdcedaeadbdb94
microdetails <lois@micro-details.com>
2026-07-27 14:44:06 +0300

    google analytic added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,6 +23,16 @@
         @inertiaHead
 
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-125PY1JPT2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-125PY1JPT2');
+        </script>        
         
     </head>
     <body class="font-sans antialiased">
